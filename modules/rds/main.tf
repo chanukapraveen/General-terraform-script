4687ddcd9d350b502e440aa0aa153d7a66acc2b5
chanukapraveen <97213247+chanukapraveen@users.noreply.github.com>
2024-07-30 14:21:58 +0530

    Add files via upload

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,1 +1,18 @@
-main
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated_storage
+  multi_az = var.multi_az
+  backup_retention_period = var.backup_retention_period
+  db_subnet_group_name = var.subnet_group_name
+  identifier_prefix = var.identifier_prefix
+  network_type = var.network_type
+  vpc_security_group_ids = ["${aws_security_group.main.id}"]
+  db_name              = var.db_name
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  username             = var.username
+  password             = var.password
+  parameter_group_name = var.parameter_group_name
+  port = var.port
+  depends_on = [ aws_db_subnet_group.default ]
+}
---
+++ modules/rds/varibales.tf
@@ -0,0 +1,74 @@
+variable "allocated_storage" {
+
+}
+
+
+variable "multi_az" {
+
+}
+
+variable "backup_retention_period" {
+
+}
+
+
+variable "identifier_prefix" {
+
+}
+
+variable "network_type" {
+
+}
+
+
+variable "db_name" {
+
+}
+
+variable "engine" {
+
+}
+
+variable "engine_version" {
+
+}
+
+variable "instance_class" {
+
+}
+
+variable "username" {
+
+}
+
+variable "password" {
+
+}
+
+variable "parameter_group_name" {
+
+}
+
+variable "port" {
+
+}
+
+variable "vpc_id" {
+
+}
+
+variable "cidr_blocks" {
+
+}
+
+variable "tags" {
+
+}
+
+variable "subnet_group_name" {
+
+}
+
+variable "subnet_ids" {
+
+}
